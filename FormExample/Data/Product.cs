2ec5645bde6467592a4db5b16386870716ee8dba
Red Wei <hevilhuy2@gmail.com>
2021-07-14 18:30:19 +0700

    Added basic form example.

---
+++ FormExample/Data/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormExample.Data
+{
+    public class Product
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Range(100, double.MaxValue)]
+        public double? Price { get; set; }
+    }
+}
